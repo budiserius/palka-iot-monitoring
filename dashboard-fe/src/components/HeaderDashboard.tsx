679cd3239348c20906e0140ae29d4b5aa1221a8f
budiserius <budiportofolio@gmail.com>
2026-02-06 20:50:36 +0700

    update: header

--- dashboard-fe/src/components/HeaderDashboard.tsx
+++ dashboard-fe/src/components/HeaderDashboard.tsx
@@ -1,6 +1,6 @@
 export default function HeaderDashboard() {
   return (
-    <h1 className="flex h-30 w-full items-center justify-center border-b-2 bg-black text-center text-5xl font-bold text-white shadow-md max-md:h-16 max-md:text-[24px] dark:bg-white dark:text-black">
+    <h1 className="flex h-30 w-full items-center justify-center border-b-2 bg-black text-center text-5xl font-bold text-white shadow-md max-md:h-16 max-md:text-[24px]">
       Dashboard IoT Management
     </h1>
   );
