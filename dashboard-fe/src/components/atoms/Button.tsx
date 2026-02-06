e0fb083a759ecab11ccdb557d60ede73aaa4537b
budiserius <budiportofolio@gmail.com>
2026-02-06 16:21:21 +0700

    feat: button components

---
+++ dashboard-fe/src/components/atoms/Button.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+  onClick?: () => void;
+};
+
+export default function Button({
+  children,
+  onClick = () => {
+    alert("Button Clicked");
+  },
+}: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="flex h-12 w-48 items-center justify-between rounded-md border-2 border-white bg-black px-6 text-white hover:border-black hover:bg-white hover:text-black"
+    >
+      {children}
+    </button>
+  );
+}
